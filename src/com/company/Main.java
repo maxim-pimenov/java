92a5593daceacb54a495dc62eb1432e463adb7a5
maixm-pimenov <maxim.pimenovyt724@gmail.com>
2019-11-30 22:16:14 +0300

    * init

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 14;
+		while (a < 100) {
+			a++;
+			if(a%5==0) {
+				System.out.print("a= "+a + "\n");
+			}
+			}
+		}
+}
